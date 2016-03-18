912626555595cc6783d99b873550be77fc7e9e28
byTimo <race.timo@gmail.com>
2016-03-19 02:11:59 +0500

    Добавил несколько простых тестов для последующей реализации. Не совсем по TDD, ну и я не овер чОткий программер

---
+++ GraphAlgorithms/CyclesFinder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GraphAlgorithms
+{
+    public class CyclesFinder
+    {
+        public IEnumerable<int[]> Find(int[][] incidenceMatrix)
+        {
+            return new List<int[]>();
+        }
+    }
+}
