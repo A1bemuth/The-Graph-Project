d20df74ffd47a9436753be8594a666dc23833a26
byTimo <race.timo@gmail.com>
2016-03-19 14:38:17 +0500

    Начата разработка класса StartingSeparator, перый зелёный тест

---
+++ GraphAlgorithms/StartingSeparator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GraphAlgorithms
+{
+    internal class StartingSeparator
+    {
+        internal IEnumerable<int[]> Paths { get; }
+        internal IEnumerable<int[]> Segments { get; }
+
+        internal StartingSeparator(int[][] incedenceMatrix)
+        {
+            Paths = new List<int[]>();
+            Segments = new List<int[]>();
+        }
+
+        internal void Separate()
+        {
+        }
+    }
+}
---
+++ GraphAlgorithms/Tests/StartingSeparatorTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace GraphAlgorithms.Tests
+{
+    [TestFixture]
+    public class StartingSeparatorTester
+    {
+        // 1 → 2 → 3
+        [Test]
+        public void SeparateThreeVertexGraphWithoutCycles()
+        {
+            var incedenceMatrix = new[]
+            {
+                new[] {1, 0},
+                new[] {-1, 1},
+                new[] {0, -1}
+            };
+            var separator = new StartingSeparator(incedenceMatrix);
+
+            separator.Separate();
+
+            Assert.That(separator.Paths.Count(), Is.EqualTo(0));
+            Assert.That(separator.Segments.Count(), Is.EqualTo(0));
+        }
+    }
+}
