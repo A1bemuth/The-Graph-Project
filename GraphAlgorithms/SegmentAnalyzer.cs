4e4102a878bc128b2c95d03e59f5d227a6710fed
byTimo <race.timo@gmail.com>
2016-03-20 20:43:22 +0500

    Добавил класс анализа сегментов и тест к нему

---
+++ GraphAlgorithms/SegmentAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace GraphAlgorithms
+{
+    public class SegmentAnalyzer
+    {
+        private readonly short[][] incedenceMatrix;
+
+        internal SegmentAnalyzer(short[][] incedenceMatrix)
+        {
+            this.incedenceMatrix = incedenceMatrix;
+        }
+
+        internal IEnumerable<int[]> CheckSegment(IEnumerable<int> segment)
+        {
+            return new List<int[]>();
+        }
+    }
+}
---
+++ GraphAlgorithms/Tests/SegmentAnalyzerTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace GraphAlgorithms.Tests
+{
+    [TestFixture]
+    public class SegmentAnalyzerTester
+    {
+        // 0 → 1 → 2
+        [Test]
+        public void SeparateThreeVertexGraphWithoutCycles()
+        {
+            var incedenceMatrix = new[]
+            {
+                new short[] {1, 0},
+                new short[] {-1, 1},
+                new short[] {0, -1}
+            };
+            var analyzer = new SegmentAnalyzer(incedenceMatrix);
+
+            var result = analyzer.CheckSegment(new[] {0, 1}).ToArray();
+
+            Assert.That(result.Length, Is.EqualTo(0));
+        }
+    }
+}
