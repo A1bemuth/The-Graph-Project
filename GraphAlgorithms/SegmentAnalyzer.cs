a8ebd1b14132f1fce69ef7d8c47c9403875bd87c
byTimo <race.timo@gmail.com>
2016-03-20 21:35:54 +0500

    Анализируем сегменты, никогда так не копи-пастите, нужен большой рефактор

--- GraphAlgorithms/SegmentAnalyzer.cs
+++ GraphAlgorithms/SegmentAnalyzer.cs
@@ -1,19 +1,86 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GraphAlgorithms
 {
     public class SegmentAnalyzer
     {
         private readonly short[][] incedenceMatrix;
+        private readonly bool[] visitedVertices;
+        private List<int> currentSequence;
+        private readonly List<int[]> Cycles;
+        private int currentVertex;
 
         internal SegmentAnalyzer(short[][] incedenceMatrix)
         {
             this.incedenceMatrix = incedenceMatrix;
+            visitedVertices = new bool[incedenceMatrix.Length];
+            Cycles = new List<int[]>();
         }
 
-        internal IEnumerable<int[]> CheckSegment(IEnumerable<int> segment)
+        internal IEnumerable<int[]> CheckSegment(int[] segment)
         {
-            return new List<int[]>();
+            currentSequence = new List<int>(segment.Take(segment.Length - 1));
+            foreach (var i in currentSequence)
+            {
+                visitedVertices[i] = true;
+            }
+            currentVertex = segment.Last();
+
+            InspectVertex();
+
+            return Cycles;
+        }
+
+        private void InspectVertex()
+        {
+            if (visitedVertices[currentVertex])
+            {
+                var previousVertexIndex = currentSequence
+                    .IndexOf((v, index) => v == currentVertex && index < currentSequence.Count - 1);
+                if (previousVertexIndex != -1)
+                {
+                    var cycle = currentSequence.Skip(previousVertexIndex);
+                    Cycles.Add(cycle.ToArray());
+                }
+            }
+            else
+            {
+                JumpToNextVertex();
+            }
+        }
+
+        private void JumpToNextVertex()
+        {
+            IncludeInSequence();
+            foreach (var arcIndex in FindOutgoingArcIndexes())
+            {
+                currentVertex = FindeNextVertex(arcIndex);
+                InspectVertex();
+            }
+            ExcludeLastVertexFromSequence();
+
+        }
+
+        private void IncludeInSequence()
+        {
+            visitedVertices[currentVertex] = true;
+            currentSequence.Add(currentVertex);
+        }
+
+        private IEnumerable<int> FindOutgoingArcIndexes()
+        {
+            return incedenceMatrix[currentVertex].IndexesOf(v => v == 1);
+        }
+
+        private int FindeNextVertex(int outgoingArcIndex)
+        {
+            return incedenceMatrix.IndexInColumnOf(outgoingArcIndex, v => v == -1);
+        }
+
+        private void ExcludeLastVertexFromSequence()
+        {
+            currentSequence.RemoveAt(currentSequence.Count - 1);
         }
     }
 }
