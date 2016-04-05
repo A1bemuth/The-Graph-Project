9eaaf575f80c46a67b60fd691e85a7b92aa46b00
byTimo <race.timo@gmail.com>
2016-04-05 14:46:28 +0500

    Тестовая отрисовка графа

---
+++ UI/Controls/NodeView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using GraphAlgorithms;
+using GraphAlgorithms.Geometry;
+
+namespace UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for NodeView.xaml
+    /// </summary>
+    public partial class NodeView : UserControl, INode
+    {
+        public NodeView()
+        {
+            InitializeComponent();
+            Size = new Size(25, 25);
+            Connections = new List<INode>();
+        }
+
+        public Size Size {
+            get
+            {
+                return new Size((int)Width, (int)Height);
+            }
+            set
+            {
+                Width = value.Width;
+                Height = value.Height;
+            }
+        }
+
+        public Point Location { get; set; }
+
+        public IList<INode> Connections { get; }
+
+        public INode AddChild(INode node)
+        {
+            Connections.Add(node);
+            return this;
+        }
+
+        public INode AddParent(INode node)
+        {
+            node.AddChild(this);
+            return this;
+        }
+
+        public bool Disconnect(INode node)
+        {
+            return Connections.Remove(node);
+        }
+    }
+}
--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using GraphAlgorithms;
+using GraphDataLayer;
+using UI.Controls;
 
 namespace UI
 {
@@ -7,10 +11,54 @@ namespace UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IGraph graph;
         public MainWindow()
         {
             InitializeComponent();
+            graph = new AdjacencyGraph(15)
+                .AddArrow(8, 4)
+                .AddArrow(3, 4)
+                .AddArrow(5, 10)
+                .AddArrow(0, 1)
+                .AddArrow(11, 9)
+                .AddArrow(9, 11)
+                .AddArrow(14, 7)
+                .AddArrow(1, 3)
+                .AddArrow(10, 6)
+                .AddArrow(11, 5)
+                .AddArrow(5, 11)
+                .AddArrow(8, 13)
+                .AddArrow(13, 8)
+                .AddArrow(4, 2)
+                .AddArrow(2, 0)
+                .AddArrow(9, 12)
+                .AddArrow(12, 9)
+                .AddArrow(12, 8)
+                .AddArrow(8, 12)
+                .AddArrow(7, 13)
+                .AddArrow(1, 6)
+                .AddArrow(6, 14)
+                .AddArrow(0, 5)
+                .AddArrow(9, 2)
+                .AddArrow(7, 3);
+        }
 
+        private void DrawGraph(object sender, RoutedEventArgs e)
+        {
+            var locator = new ForceVerticesLocator();
+            var nodes = Enumerable.Range(0, graph.VerticesCount)
+                .Select(i => new NodeView())
+                .ToArray();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                foreach (var neighbour in graph.GetNeighbours(i))
+                {
+                    nodes[i].AddChild(nodes[neighbour]);
+                }
+                locator.AddNode(nodes[i]);
+            }
+
+            GraphView.VerticesLocator = locator;
         }
     }
 }
