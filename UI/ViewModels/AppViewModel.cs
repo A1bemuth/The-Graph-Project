4ec6b315ae0ac263b1b8a36cab105ff1329fab2e
byTimo <race.timo@gmail.com>
2016-04-15 13:16:35 +0500

    Небольшие изменения интерфейса

--- UI/ViewModels/AppViewModel.cs
+++ UI/ViewModels/AppViewModel.cs
@@ -43,35 +43,11 @@ namespace UI.ViewModels
 
         private void LoadGraph(object parameter)
         {
-            Graph = new AdjacencyGraph(12)
-                .AddArrow(0, 6)
-                .AddArrow(0, 7)
-                .AddArrow(1, 0)
-                .AddArrow(1, 3)
-                .AddArrow(1, 4)
-                .AddArrow(2, 3)
-                .AddArrow(2, 11)
-                .AddArrow(3, 11)
-                .AddArrow(4, 11)
-                .AddArrow(5, 11)
-                .AddArrow(6, 5)
-                .AddArrow(7, 1)
-                .AddArrow(7, 2)
-                .AddArrow(8, 11)
-                .AddArrow(9, 10)
-                .AddArrow(9, 11)
-                .AddArrow(10, 11)
-                .AddArrow(11, 0)
-                .AddArrow(11, 1)
-                .AddArrow(11, 2)
-                .AddArrow(11, 3)
-                .AddArrow(11, 4)
-                .AddArrow(11, 5)
-                .AddArrow(11, 6)
-                .AddArrow(11, 7)
-                .AddArrow(11, 8)
-                .AddArrow(11, 9)
-                .AddArrow(11, 10);
+            Graph = new AdjacencyListGraph(4)
+                .AddArrow(0, 3)
+                .AddArrow(0, 1)
+                .AddArrow(1, 2)
+                .AddArrow(2, 0);
         }
 
         [NotifyPropertyChangedInvocator]
