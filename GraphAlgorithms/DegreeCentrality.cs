6d6d661311aea9c86864e10bca80e4ac234cd924
Dmitry Kostarev <albemuth1221@gmail.com>
2016-04-19 14:48:17 +0500

    Смежность + правки

---
+++ GraphAlgorithms/DegreeCentrality.cs
@@ -0,0 +1,26 @@
+﻿using GraphDataLayer;
+
+namespace GraphAlgorithms
+{
+    class DegreeCentrality
+    {
+        public DegreeCentrality(Graph graph)
+        {
+            this.graph = graph;
+        }
+
+        public int GetVerticeInDegreeCentrality(int index)
+        {
+            return graph.GetIncomingVertex(index).Count;
+        }
+
+        public int GetVerticeOutDegreeCentrality(int index)
+        {
+            return graph.GetNeighbours(index).Count;
+        }
+
+        
+
+        private Graph graph;
+    }
+}
