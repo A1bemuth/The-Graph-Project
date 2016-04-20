937d02808325d53ec37609d8d486b0b062173fd1
Dmitry Kostarev <albemuth1221@gmail.com>
2016-04-20 22:44:55 +0500

    Добавил центральность + тестик

--- GraphAlgorithms/DegreeCentrality.cs
+++ GraphAlgorithms/DegreeCentrality.cs
@@ -1,26 +1,76 @@
-﻿using GraphDataLayer;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphDataLayer;
 
 namespace GraphAlgorithms
 {
-    class DegreeCentrality
+    public class DegreeCentrality
     {
         public DegreeCentrality(Graph graph)
         {
             this.graph = graph;
+            this.vertices = Enumerable.Range(0, graph.VerticesCount).ToList();
         }
 
-        public int GetVerticeInDegreeCentrality(int index)
+        public int GetVerticeIndegreeCentrality(int index)
         {
             return graph.GetIncomingVertex(index).Count;
         }
 
-        public int GetVerticeOutDegreeCentrality(int index)
+        public int GetVerticeOutdegreeCentrality(int index)
         {
             return graph.GetNeighbours(index).Count;
         }
 
-        
+        public int GetGraphIndegreeCentrality()
+        {
+            return Centrality(GetVerticeIndegreeCentrality);
+        }
 
-        private Graph graph;
+        public int GetGraphOutdegreeCentrality()
+        {
+            return Centrality(GetVerticeOutdegreeCentrality);
+        }
+
+        public double GetIndegreeStandartDeviation()
+        {
+            return GetStandartDeviation(GetVerticeIndegreeCentrality);
+        }
+
+        public double GetOutdegreeStandartDeviation()
+        {
+            return GetStandartDeviation(GetVerticeOutdegreeCentrality);
+        }
+
+        private double GetStandartDeviation(Func<int, int> getVerticeCentrality )
+        {
+            var avg = MeanCentrality(getVerticeCentrality);
+            var dispersion = (double) 1/vertices.Count * vertices.Sum(c => Math.Pow(c - avg, 2));
+            return Math.Sqrt(dispersion);
+        }
+
+        private double MeanCentrality(Func<int, int> getVerticeCentrality)
+        {
+            return vertices.Average();
+        }
+
+        private int Centrality(Func<int, int> getVerticeCentrality)
+        {
+            var verticesCount = graph.VerticesCount;
+            var centralities = vertices
+                .Select(getVerticeCentrality)
+                .ToList();
+
+            var mostCentral = centralities.Max();
+            var sum = centralities.Sum(c => mostCentral - c);
+            var denominator = (verticesCount - 1)*(verticesCount - 2);
+
+            return sum/denominator;
+        }
+
+        private List<int> vertices;
+
+        private readonly Graph graph;
     }
 }
---
+++ Tests/AlgorithmsTests/DegreeCentralityTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GraphDataLayer;
+using GraphAlgorithms;
+using NUnit.Framework;
+
+namespace Tests.AlgorithmsTests
+{
+    [TestFixture]
+    class DegreeCentralityTests
+    {
+        [Test]
+        public void TestMaximumCentrality() //ну и хватит
+        {
+            var graph = FullCentralGraph();
+            var centrality = new DegreeCentrality(graph);
+
+            var graphIn = centrality.GetGraphIndegreeCentrality();
+            var graphOut = centrality.GetGraphOutdegreeCentrality();
+            var verticeIn = centrality.GetVerticeIndegreeCentrality(0);
+            var verticeOut = centrality.GetVerticeOutdegreeCentrality(0);
+            Assert.That(graphIn, Is.EqualTo(graphOut).And.EqualTo(1));
+            Assert.That(verticeIn, Is.EqualTo(verticeOut).And.EqualTo(3));
+        }
+
+        private static Graph FullCentralGraph()
+        {
+            var graph = (Graph) new AdjacencyGraph(4);
+            for (int i = 1; i < 4; i++)
+            {
+                graph = graph.AddArrow(0, i);
+                graph = graph.AddArrow(i, 0);
+            }
+            return graph;
+        }
+    }
+}
